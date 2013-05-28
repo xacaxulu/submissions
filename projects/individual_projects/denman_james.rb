0ee795358254930fa9ebedaa5a3290ad81db31df
xacaxulu <denmanjd@gmail.com>
2013-05-28 14:02:53 -0600

    denman;

---
+++ projects/individual_projects/denman_james.rb
@@ -0,0 +1,10 @@
+{
+  :last_name => "DENMAN",
+  :first_name => "JAMES",
+  :project_name => "KronSong",
+  :project_description => "Something with music/spotify",
+  :technologies => ['slim'],
+  :integrations => ['spotify']
+  :repo => 'http://github.com/username/repo_name',
+  :heroku_account => 'denmanjd@gmail.com'
+}
