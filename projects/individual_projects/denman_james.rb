311d7c76560c625f524399c1a723bda051926910
xacaxulu <denmanjd@gmail.com>
2013-05-29 15:15:59 -0600

    update

--- projects/individual_projects/denman_james.rb
+++ projects/individual_projects/denman_james.rb
@@ -2,9 +2,9 @@
   :last_name => "DENMAN",
   :first_name => "JAMES",
   :project_name => "KronSong",
-  :project_description => "Something with music/spotify",
+  :project_description => "Notifications on upcoming concerts based on Spotify selections",
   :technologies => ['slim'],
-  :integrations => ['spotify']
+  :integrations => ['spotify', 'songkick']
   :repo => 'http://github.com/username/repo_name',
   :heroku_account => 'denmanjd@gmail.com'
 }
